4c49c6cff329d41cf13e0795338b761157932d62
Manickavasagam97 <manickavasagam2297@gmail.com>
2023-08-08 16:12:36 +0530

    emp file added

---
+++ Employee/src/main/java/edu/qsp/dto/Employee.java
@@ -0,0 +1,65 @@
+package edu.qsp.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private double sal;
+	private long contactNo;
+	private String job;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSal() {
+		return sal;
+	}
+
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+
+	public long getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(long contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", sal=" + sal + ", contactNo=" + contactNo + ", job=" + job
+				+ "]";
+	}
+
+}
